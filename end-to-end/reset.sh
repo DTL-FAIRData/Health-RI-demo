71f6179c291875057e44c1723bd2e9a582c02256
Rajaram Kaliyaperumal <r.kaliyaperumal@lumc.nl>
2017-11-23 13:16:08 +0100

    script to open new chrome tab is added

--- end-to-end/reset.sh
+++ end-to-end/reset.sh
@@ -13,16 +13,19 @@ curl -X DELETE "$bGraphURL/$bGraphNamespace"
 printf "\n\n------------CREATING namespace $bGraphNamespace------------"
 curl -v -X POST --data-binary @test.xml --header 'Content-Type:application/xml' $bGraphURL
 
-printf "\n\n------------DELETING $gcCookies Cookies attempt 1------------"
-cd ~/.config/google-chrome/Default/
-sqlite3 Cookies "DELETE FROM $gcCookiesFileName WHERE host_key LIKE '%$gcCookies%';"
+#printf "\n\n------------DELETING $gcCookies Cookies attempt 1------------"
+#cd ~/.config/google-chrome/Default/
+#sqlite3 Cookies "DELETE FROM $gcCookiesFileName WHERE host_key LIKE '%$gcCookies%';"
 
-printf "\n\n------------DELETING $gcCookies Cookies attempt 2------------"
-cd ~/.config/google-chrome/Profile\ 1/
-sqlite3 Cookies "DELETE FROM $gcCookiesFileName WHERE host_key LIKE '%$gcCookies%';"
+#printf "\n\n------------DELETING $gcCookies Cookies attempt 2------------"
+#cd ~/.config/google-chrome/Profile\ 1/
+#sqlite3 Cookies "DELETE FROM $gcCookiesFileName WHERE host_key LIKE '%$gcCookies%';"
 
 printf "\n\n------------DELETING agraph repos $aGraphRepo------------"
 curl -X DELETE "$aGraphUrl/repositories/$aGraphRepo" -u test:xyzzy
 
 printf "\n\n------------CREATING agraph repos $aGraphRepo------------"
 curl -X PUT "$aGraphUrl/repositories/$aGraphRepo" -u test:xyzzy
+
+printf "\n\n------------OPENING new incognito mode------------"
+google-chrome --incognito --args "http://localhost:8505"
