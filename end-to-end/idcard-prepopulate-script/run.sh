192663cdc1acab10ad75f392354bafe8a31b7963
shamanou <mandarijnopw8@gmail.com>
2017-11-30 09:36:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- end-to-end/idcard-prepopulate-script/run.sh
+++ end-to-end/idcard-prepopulate-script/run.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #printf "============== Running reset script to init demo env =============="
 #sh reset.sh
 
@@ -18,3 +19,20 @@ printf "============== Setup triplestores ==========================="
 
 cd /usr/src/app
 sh reset.sh
+=======
+#printf "============== Running reset script to init demo env =============="
+#sh reset.sh
+
+cd script/src
+biobankfdpURL="http://localhost:8501/fdp"
+registryfdpURL="http://localhost:8500/fdp"
+simpleServerURL="http://localhost:8503/"
+
+printf "============== POSTING Idcard's biobank metadata =============="
+python3 idcard.py biobank $biobankfdpURL $simpleServerURL  
+curl -X GET --header 'Accept: */*' 'https://localhost:8506/fse/submitFdp?fdp=$biobankfdpURL'
+
+printf "============== POSTING Idcard's registry metadata =============="
+python3 idcard.py registry $registryfdpURL $simpleServerURL
+curl -X GET --header 'Accept: */*' 'https://localhost:8506/fse/submitFdp?fdp=$registryfdpURL'
+>>>>>>> Stashed changes
